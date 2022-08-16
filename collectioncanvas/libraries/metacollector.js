0a1cade36469f83486ebce1ac30c587dca1890b0
Julien <juliendorra@juliendorra.com>
2022-08-16 10:08:54 +0200

    Add deterministic random function pre-seeded with walletAddress
    Using macmcmeans's Alea implementation to offer a deterministic random function.
    Creative coders can use metacollector.random() instead of Math.random()
    And metacollector.range(x,  y) instead of the global random(x, y) from p5js

--- collectioncanvas/libraries/metacollector.js
+++ collectioncanvas/libraries/metacollector.js
@@ -58,16 +58,6 @@ if (document.readyState === 'loading') {
     init();
 }
 
-function xmur3(str) {
-    for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
-        h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
-        h = h << 13 | h >>> 19;
-    } return function () {
-        h = Math.imul(h ^ (h >>> 16), 2246822507);
-        h = Math.imul(h ^ (h >>> 13), 3266489909);
-        return (h ^= h >>> 16) >>> 0;
-    }
-}
 
 function init() {
 
@@ -490,6 +480,7 @@ function paintCollectionUsingClone(metacollector) {
 
     clonedMetacollector.seed = xmur3(metacollector.walletAddress)()
 
+    clonedMetacollector.random = new aleaPRNG(metacollector.walletAddress)
 
     paintCollection(clonedMetacollector, clonedMetacollector.random)
 }
@@ -641,3 +632,194 @@ function createP5Image(bitmap) {
     }
 
 })(this);
+
+// MurmurHash3
+
+function xmur3(str) {
+    for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+        h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+        h = h << 13 | h >>> 19;
+    } return function () {
+        h = Math.imul(h ^ (h >>> 16), 2246822507);
+        h = Math.imul(h ^ (h >>> 13), 3266489909);
+        return (h ^= h >>> 16) >>> 0;
+    }
+}
+
+// Alea
+// original article https://web.archive.org/web/20110608011113/http://baagoe.com/en/RandomMusings/javascript/
+// mirror https://github.com/nquinlan/better-random-numbers-for-javascript-mirror
+// alt implementation: https://github.com/davidbau/seedrandom/blob/released/lib/alea.js
+// alt implementation https://github.com/coverslide/node-alea/blob/master/alea.js
+// this implementation https://github.com/macmcmeans/aleaPRNG/blob/master/aleaPRNG-1.1.js
+
+function aleaPRNG() {
+    return (function (args) {
+        "use strict";
+
+        const version = 'aleaPRNG 1.1.0';
+
+        var s0
+            , s1
+            , s2
+            , c
+            , uinta = new Uint32Array(3)
+            , initialArgs
+            , mashver = ''
+            ;
+
+        /* private: initializes generator with specified seed */
+        function _initState(_internalSeed) {
+            var mash = Mash();
+
+            // internal state of generator
+            s0 = mash(' ');
+            s1 = mash(' ');
+            s2 = mash(' ');
+
+            c = 1;
+
+            for (var i = 0; i < _internalSeed.length; i++) {
+                s0 -= mash(_internalSeed[i]);
+                if (s0 < 0) { s0 += 1; }
+
+                s1 -= mash(_internalSeed[i]);
+                if (s1 < 0) { s1 += 1; }
+
+                s2 -= mash(_internalSeed[i]);
+                if (s2 < 0) { s2 += 1; }
+            }
+
+            mashver = mash.version;
+
+            mash = null;
+        };
+
+        /* private: dependent string hash function */
+        function Mash() {
+            var n = 4022871197; // 0xefc8249d
+
+            var mash = function (data) {
+                data = data.toString();
+
+                // cache the length
+                for (var i = 0, l = data.length; i < l; i++) {
+                    n += data.charCodeAt(i);
+
+                    var h = 0.02519603282416938 * n;
+
+                    n = h >>> 0;
+                    h -= n;
+                    h *= n;
+                    n = h >>> 0;
+                    h -= n;
+                    n += h * 4294967296; // 0x100000000      2^32
+                }
+                return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+            };
+
+            mash.version = 'Mash 0.9';
+            return mash;
+        };
+
+
+        /* private: check if number is integer */
+        function _isInteger(_int) {
+            return parseInt(_int, 10) === _int;
+        };
+
+        /* public: return a 32-bit fraction in the range [0, 1]
+        This is the main function returned when aleaPRNG is instantiated
+        */
+        var random = function () {
+            var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
+
+            s0 = s1;
+            s1 = s2;
+
+            return s2 = t - (c = t | 0);
+        };
+
+        /* public: return a 53-bit fraction in the range [0, 1] */
+        random.fract53 = function () {
+            return random() + (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
+        };
+
+        /* public: return an unsigned integer in the range [0, 2^32] */
+        random.int32 = function () {
+            return random() * 0x100000000; // 2^32
+        };
+
+        /* public: advance the generator the specified amount of cycles */
+        random.cycle = function (_run) {
+            _run = typeof _run === 'undefined' ? 1 : +_run;
+            if (_run < 1) { _run = 1; }
+            for (var i = 0; i < _run; i++) { random(); }
+        };
+
+        /* public: return inclusive range */
+        random.range = function () {
+            var loBound
+                , hiBound
+                ;
+
+            if (arguments.length === 1) {
+                loBound = 0;
+                hiBound = arguments[0];
+
+            } else {
+                loBound = arguments[0];
+                hiBound = arguments[1];
+            }
+
+            if (arguments[0] > arguments[1]) {
+                loBound = arguments[1];
+                hiBound = arguments[0];
+            }
+
+            // return integer
+            if (_isInteger(loBound) && _isInteger(hiBound)) {
+                return Math.floor(random() * (hiBound - loBound + 1)) + loBound;
+
+                // return float
+            } else {
+                return random() * (hiBound - loBound) + loBound;
+            }
+        };
+
+        /* public: initialize generator with the seed values used upon instantiation */
+        random.restart = function () {
+            _initState(initialArgs);
+        };
+
+        /* public: seeding function */
+        random.seed = function () {
+            _initState(Array.prototype.slice.call(arguments));
+        };
+
+        /* public: show the version of the RNG */
+        random.version = function () {
+            return version;
+        };
+
+        /* public: show the version of the RNG and the Mash string hasher */
+        random.versions = function () {
+            return version + ', ' + mashver;
+        };
+
+        // when no seed is specified, create a random one from Windows Crypto (Monte Carlo application) 
+        if (args.length === 0) {
+            window.crypto.getRandomValues(uinta);
+            args = [uinta[0], uinta[1], uinta[2]];
+        };
+
+        // store the seed used when the RNG was instantiated, if any
+        initialArgs = args;
+
+        // initialize the RNG
+        _initState(args);
+
+        return random;
+
+    })(Array.prototype.slice.call(arguments));
+};
